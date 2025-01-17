b9467c7bc9dbd2ae44d4932996daa30b63e59bf8
doruk.tekel <doruk.tekel@bilgeadam.com>
2025-01-17 22:11:59 +0300

    deployment settings created

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
@@ -18,6 +19,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = envVariables.PORT || 4005;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
@@ -27,6 +29,13 @@ app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/post", postRouter);
 app.use("/api/v1/notification", notificationRouter);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, (req, res) => {
   try {
